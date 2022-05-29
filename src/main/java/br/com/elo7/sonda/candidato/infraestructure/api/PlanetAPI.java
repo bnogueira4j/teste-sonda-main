0bebc59255f30100008ad7c9c25786b70148897d
Brunno Nogueira <brunno.nogueira4j@gmail.com>
2022-05-29 11:32:45 -0300

    feat: configre openapi-ui and docker build stages in dockerfile

--- src/main/java/br/com/elo7/sonda/candidato/infraestructure/api/PlanetAPI.java
+++ src/main/java/br/com/elo7/sonda/candidato/infraestructure/api/PlanetAPI.java
@@ -4,6 +4,10 @@ import br.com.elo7.sonda.candidato.application.planet.common.PlanetOutput;
 import br.com.elo7.sonda.candidato.infraestructure.planet.models.CreatePlanetApiInput;
 import br.com.elo7.sonda.candidato.infraestructure.planet.models.PlanetApiInput;
 import br.com.elo7.sonda.candidato.infraestructure.planet.models.PlanetApiOutput;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.responses.ApiResponse;
+import io.swagger.v3.oas.annotations.responses.ApiResponses;
+import io.swagger.v3.oas.annotations.tags.Tag;
 import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -16,23 +20,47 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import java.util.List;
 
 @RequestMapping(value = "planet")
+@Tag(name = "Planet")
 public interface PlanetAPI {
 
-    @GetMapping("/{id}")
+    @GetMapping(value = "/{id}",
+            consumes = MediaType.ALL_VALUE,
+            produces = MediaType.APPLICATION_JSON_VALUE)
+    @Operation(summary = "Get a planet by id")
+    @ApiResponses(value = {
+            @ApiResponse(responseCode = "200", description = "Get planet by id successfully"),
+            @ApiResponse(responseCode = "500", description = "An internal server error was thrown")
+    })
     PlanetApiOutput getById(@PathVariable int id);
 
-    @GetMapping
+    @GetMapping(
+            produces = MediaType.APPLICATION_JSON_VALUE)
+    @Operation(summary = "List all planets")
+    @ApiResponses(value = {
+            @ApiResponse(responseCode = "200", description = "Listed all planets successfully"),
+            @ApiResponse(responseCode = "500", description = "An internal server error was thrown")
+    })
     ResponseEntity<List<PlanetOutput>> findAll();
 
     @PostMapping(
             consumes = MediaType.APPLICATION_JSON_VALUE,
             produces = MediaType.APPLICATION_JSON_VALUE
     )
+    @Operation(summary = "Create a new planet")
+    @ApiResponses(value = {
+            @ApiResponse(responseCode = "201", description = "Created planet successfully"),
+            @ApiResponse(responseCode = "500", description = "An internal server error was thrown")
+    })
     ResponseEntity<?> register(@RequestBody CreatePlanetApiInput input);
 
     @PutMapping(
             consumes = MediaType.APPLICATION_JSON_VALUE,
             produces = MediaType.APPLICATION_JSON_VALUE
     )
+    @Operation(summary = "Update a planet")
+    @ApiResponses(value = {
+            @ApiResponse(responseCode = "200", description = "Updated a planet successfully"),
+            @ApiResponse(responseCode = "500", description = "An internal server error was thrown")
+    })
     ResponseEntity<?> update(@RequestBody PlanetApiInput input);
 }
